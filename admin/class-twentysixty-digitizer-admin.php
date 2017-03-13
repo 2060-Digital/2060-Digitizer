6e877ee1a9cef120981cbf34f611b70fafb9f335
Ryan Benhase <rbenhase@2060digital.com>
2017-03-13 16:56:44 -0400

    Remove user replacement logic

--- admin/class-twentysixty-digitizer-admin.php
+++ admin/class-twentysixty-digitizer-admin.php
@@ -208,7 +208,6 @@ class Twentysixty_Digitizer_Admin {
   	</form>
   	</div>
   	<?php
-  
   }
   
   
@@ -220,35 +219,13 @@ class Twentysixty_Digitizer_Admin {
    */
   public function maybe_run_update() {
     
+    
+    return; // remove this to run updates
+    
     $digitizer_version = get_option( "twentysixty-digitizer-version" );
     
     if ( empty( $digitizer_version ) || version_compare( $this->version, $digitizer_version ) === 1 ) {     
         // Anything that needs to be run once when the plugin is updated
-    
-      // Add/remove user accounts
-      $user = get_user_by( "email", "achalfant@2060digital.com" );      
-
-      if ( $user == false ) 
-        $user = get_user_by( "email", "aringo@2060digital.com" );            
-                
-      if ( $user != false ) {
-        $user->user_nicename = 'ttorres';
-        $user->display_name = 'Tahnee Torres';
-        $user->user_email = 'ttorres@2060digital.com';  
-        
-        $user_id = wp_update_user( $user );
-        
-        if ( !is_wp_error( $user_id ) ) {
-          update_user_meta( $user->ID, 'nickname', 'ttorres' );
-          update_user_meta( $user->ID, 'first_name', 'Tahnee' );
-          update_user_meta( $user->ID, 'last_name', 'Torres' );
-          
-          global $wpdb;
-          
-          $wpdb->update( $wpdb->users, array( 'user_login' => 'ttorres' ), array( 'ID' => $user_id ) );
-        }       
-      }          
-      update_option( "twentysixty-digitizer-version", $this->version );   
     }
 	}
 }
--- includes/class-twentysixty-digitizer.php
+++ includes/class-twentysixty-digitizer.php
@@ -79,7 +79,7 @@ class Twentysixty_Digitizer {
 
   	$this->main_file = $main_file;
 		$this->plugin_name = 'twentysixty-digitizer';
-		$this->version = '1.0.5';
+		$this->version = '1.0.6';
 
 		$this->load_dependencies();
 		$this->set_locale();
